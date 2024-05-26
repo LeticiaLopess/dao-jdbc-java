041634b04c9a821b885beda5e9c16c073cbdddbd
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2024-05-26 00:28:52 -0300

    findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -3,7 +3,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import db.DB;
 import db.DbException;
 import model.dao.SellerDao;
@@ -53,9 +57,7 @@ public class SellerDaoJDBC implements SellerDao{
 			
 			if (rs.next()) {
 				Department dep = instantiateDepartment(rs);
-				
 				Seller obj = instantiateSeller(rs, dep);
-				
 				return obj;
 			}
 			
@@ -96,6 +98,54 @@ public class SellerDaoJDBC implements SellerDao{
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*, department.Name as DepName "
+					+ "FROM seller INNER JOIN department ON seller.DepartmentId = department.Id "
+					+ "WHERE DepartmentId = ? "
+					+ "ORDER BY Name");
+			
+			st.setInt(1, department.getId());
+			rs = st.executeQuery(); 
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			
+			while (rs.next()) { // percorre enquanto tiver resultado pois pode vir mais de um
+				
+/*
+para não ocorrer a repetição de instanciação de departamento, 
+fizemos esse trecho que verifica se o departamento já consta 
+dentro de um Map para retornar o existente e não criar mais,
+pois farão referência para o mesmo objeto de departamento.
+*/
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);
+			}
+			return list;
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+			
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+		
 	}
 
 }
