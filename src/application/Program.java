d8e0579db19a38ad3d612e5d8a3263190b71458a
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2024-05-26 00:00:19 -0300

    reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,6 +11,8 @@ public class Program {
 	public static void main(String[] args) {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao(); // assim, o seu programa não conhece a implementação, conhece somente a interface. É também uma forma de fazer injeção de dependências sem explicitar a implementação.  
+		
+		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
