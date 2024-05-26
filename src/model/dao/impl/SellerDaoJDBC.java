0e054f1f25aa7de7a5df360f2b3d1f6ca254f4bb
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2024-05-26 00:36:54 -0300

    findAll implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -7,14 +7,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import db.DB;
 import db.DbException;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-public class SellerDaoJDBC implements SellerDao{
+public class SellerDaoJDBC implements SellerDao {
 	
 	private Connection conn;
 	
@@ -95,9 +94,44 @@ public class SellerDaoJDBC implements SellerDao{
 	}
 
 	@Override
-	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Seller> findAll() { // busca todos os vendedores com nome do departamento ordenando por nome
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			st = conn.prepareStatement(
+					"SELECT seller.*, department.Name as DepName "
+					+ "FROM seller INNER JOIN department ON seller.DepartmentId = department.Id "
+					+ "ORDER BY Name");
+
+			rs = st.executeQuery(); 
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			
+			while (rs.next()) {
+				
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);
+			}
+			return list;
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+			
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+		
 	}
 
 	@Override
@@ -118,14 +152,8 @@ public class SellerDaoJDBC implements SellerDao{
 			List<Seller> list = new ArrayList<>();
 			Map<Integer, Department> map = new HashMap<>();
 			
-			while (rs.next()) { // percorre enquanto tiver resultado pois pode vir mais de um
+			while (rs.next()) {
 				
-/*
-para não ocorrer a repetição de instanciação de departamento, 
-fizemos esse trecho que verifica se o departamento já consta 
-dentro de um Map para retornar o existente e não criar mais,
-pois farão referência para o mesmo objeto de departamento.
-*/
 				Department dep = map.get(rs.getInt("DepartmentId"));
 				
 				if (dep == null) {
